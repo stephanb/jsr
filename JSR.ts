b20bc0624b8f0ce03ea075d92a8339ee722c0dab
Mateusz <mateusz.koteja@enp.pl>
2019-02-08 23:19:01 +0100

    [v2.0.0] add comments to JSR, change method of storing available modules to Array

--- JSR.ts
+++ JSR.ts
@@ -7,26 +7,47 @@ interface IConfig {
 export class JSR {
   private fModules: Module[] = [];
 
-  private static fAvailableModules: Record<string, ModuleConstructor> = {};
+  /** Holds collection of available modules to use */
+  private static fAvailableModules: ModuleConstructor[] = [];
 
+  /**
+   * Returns copy of all available modules to use.
+   * Returns it as friendly map of module's name (key) and constructor (value).
+   */
   public static get modules (): Record<string, ModuleConstructor> {
-    return { ...this.fAvailableModules };
+    return this.fAvailableModules.reduce(
+      (map, module) => { map[module.name] = module; return map; },
+      {} as Record<string, ModuleConstructor>,
+    );
   }
 
+  /**
+   * Adds module's constructor to list of available modules to use.
+   *
+   * @param module module's constructor
+   */
   public static use (module: ModuleConstructor): void {
     if (module.name in JSR.fAvailableModules) {
       throw new Error(`Module ${module.name} is already in use`);
     }
 
-    JSR.fAvailableModules[module.name] = module;
+    JSR.fAvailableModules.push(module);
   }
 
+  /**
+   * Creates JSR instance.
+   *
+   * @param config JSR configuration
+   */
   constructor (config: IConfig) {
-    const modules: ModuleConstructor[] = config.modules || Array.from(Object.values(JSR.modules));
-    this.fModules = this.collectModules(modules);
+    const modules: ModuleConstructor[] = config.modules || JSR.fAvailableModules;
+    this.fModules = this.buildModules(modules);
   }
 
-  private collectModules (moduleList: ModuleConstructor[]): Module[] {
+  /**
+   * Converts modules' constructors into their instances.
+   */
+  private buildModules (moduleList: ModuleConstructor[]): Module[] {
     return moduleList.map(moduleConstructor => new moduleConstructor());
   }
 }
