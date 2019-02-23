44fc65e0e7a901cedd83105817355e706ac59730
Soanvig <soanvig@gmail.com>
2019-02-23 23:43:31 +0100

    [v2.0.0] fix Jest tests by ignoring css-like modules

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
   preset: 'ts-jest',
   moduleNameMapper: {
     '^~/(.*)$': '<rootDir>/src/$1',
+    '\\.(css|scss|less|sass)$': '<rootDir>/src/mocks/cssMock.ts',
   },
 };
---
+++ src/mocks/cssMock.ts
@@ -0,0 +1,5 @@
+/**
+ * This file is empty intentionally.
+ * CSS during testing is not required,
+ * so it's mapped to that empty file.
+ */
