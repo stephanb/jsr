fc34ff91603eecbe1c3205d6297916b212c14969
Soanvig <soanvig@gmail.com>
2019-03-01 22:51:36 +0100

    [v2.0.0] add tests for helpers/values

---
+++ src/helpers/values.spec.ts
@@ -0,0 +1,36 @@
+import { getClosestValueIndex, realToRatio, ratioToReal } from '~/helpers/values';
+import { TValueReal, TValueRatio } from '~/types';
+
+describe('helpers/values', () => {
+  describe('getClosestValueIndex', () => {
+    it('should return closest value index in simple scenarios', () => {
+      expect(getClosestValueIndex([0, 1, 5], 4)).toBe(2);
+      expect(getClosestValueIndex([5, 5, 5], 5)).toBe(0);
+      expect(getClosestValueIndex([-2, 0, 5], -2)).toBe(0);
+      expect(getClosestValueIndex([0, 0, 5], 4)).toBe(2);
+      expect(getClosestValueIndex([], 4)).toBe(0);
+    });
+
+    it('should return closest value index in floating point scenarios', () => {
+      expect(getClosestValueIndex([0.0, 1.6, 1.9], 1.7)).toBe(1);
+      expect(getClosestValueIndex([0.0, 1.6, 1.9], 1.5)).toBe(1);
+      expect(getClosestValueIndex([0.0, 1.6, 1.9], 0.5)).toBe(0);
+    });
+  });
+
+  describe('realToRatio', () => {
+    it('should convert real number to ratio number', () => {
+      expect(realToRatio(0, 100, 50 as TValueReal)).toBeCloseTo(0.5);
+      expect(realToRatio(0, 1, 0.5 as TValueReal)).toBeCloseTo(0.5);
+      expect(realToRatio(0, 100, 200 as TValueReal)).toBeCloseTo(2);
+    });
+  });
+
+  describe('ratioToReal', () => {
+    it('should convert ratio number to real number', () => {
+      expect(ratioToReal(0, 100, 0.5 as TValueRatio)).toBeCloseTo(50);
+      expect(ratioToReal(0, 1, 0.5 as TValueRatio)).toBeCloseTo(0.5);
+      expect(ratioToReal(0, 100, 2 as TValueRatio)).toBeCloseTo(200);
+    });
+  });
+});
